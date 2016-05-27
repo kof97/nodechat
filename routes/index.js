f6b17eca9b12440d4510e760a0a18cc810cbcce0
kof97 <1048434786@qq.com>
2016-05-27 18:39:13 +0800

    complete

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  	res.render('index', { title: 'node chat' });
+  res.render('index', { title: 'node chat' });
 });
 
 module.exports = router;
